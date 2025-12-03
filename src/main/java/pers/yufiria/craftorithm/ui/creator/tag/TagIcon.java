7a5ce2378603b540743c54c1aa9b2aa45874c279
Zenko <101385666+ZenkoKasai@users.noreply.github.com>
2025-12-02 23:22:19 -0600

    Update TagIcon.java

--- src/main/java/pers/yufiria/craftorithm/ui/creator/tag/TagIcon.java
+++ src/main/java/pers/yufiria/craftorithm/ui/creator/tag/TagIcon.java
@@ -1,6 +1,16 @@
 package pers.yufiria.craftorithm.ui.creator.tag;
 
 import crypticlib.ui.display.Icon;
+import crypticlib.ui.display.IconDisplay;
+import org.bukkit.Material;
 
+/**
+ * Placeholder icon used by the tag creator UI.
+ * Uses an empty AIR display by default.
+ */
 public class TagIcon extends Icon {
+
+    public TagIcon() {
+        super(new IconDisplay(Material.AIR));
+    }
 }
